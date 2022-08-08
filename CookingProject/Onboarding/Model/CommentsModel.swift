d74e275e26141cb9b51582ffd13093e844166f16
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2022-08-08 14:43:47 +0900

    Ninth Commit : data delete function, some modify

---
+++ CookingProject/Onboarding/Model/CommentsModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommentsModel.swift
+//  CookingProject
+//
+//  Created by 엄지호 on 2022/08/04.
+//
+
+import Foundation
--- CookingProject/Onboarding/Singleton/User.swift
+++
@@ -1,8 +0,0 @@
-//
-//  User.swift
-//  CookingProject
-//
-//  Created by 엄지호 on 2022/07/21.
-//
-
-import Foundation
