75165ffb5f913178f725d840a4690ef96fd9f535
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2022-08-10 00:03:56 +0900

    Tenth Commit : New RegisterView

---
+++ CookingProject/Custom Cells/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CookingProject
+//
+//  Created by 엄지호 on 2022/08/08.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
