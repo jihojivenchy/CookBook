0e221380ecc29cc6acf272a664a1f579f24933c8
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2022-08-11 00:17:34 +0900

    The Eleventh Commit : DetailViewController remodeling

---
+++ CookingProject/Custom Cells/WritePhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WritePhotoCollectionViewCell.swift
+//  CookingProject
+//
+//  Created by 엄지호 on 2022/08/10.
+//
+
+import UIKit
+
+class WritePhotoCollectionViewCell: UICollectionViewCell {
+    
+}
--- CookingProject/New Group2/NextRegisterViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NextRegisterViewController.swift
-//  CookingProject
-//
-//  Created by 엄지호 on 2022/07/13.
-//
-
-import Foundation
